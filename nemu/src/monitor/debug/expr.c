9d521ad5ff2b13de8b2c46448dbcc959762368d6
tracer-NEMU2020 <tjuics2020@163.com>
2021-08-24 06:37:32 -0700

    >  compile NEMU 3020202295 eric Linux ubuntu 5.11.0-25-generic #27-Ubuntu SMP Fri Jul 9 23:06:29 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  06:37:32 up 13:53,  1 user,  load average: 0.28, 0.08, 0.02 f43bf5e2bc3f70254721ed8bce45c0dad0d22f9

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -303,7 +303,7 @@ uint32_t eval(int p, int q, bool *success) {
        }
     int val1=0;
     if(p!=op)
-        eval(p, op - 1, success);
+        val1=eval(p, op - 1, success);
     int val2 = eval(op + 1, q, success);
     printf("val1:%d,val2:%d",val1,val2);
     switch (tokens[(int)op].type) {
