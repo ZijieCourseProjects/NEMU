b92ec6940638fe413c20fb66cc2df1b2f3ea1e73
tracer-NEMU2020 <tjuics2020@163.com>
2021-08-24 06:02:00 -0700

    >  compile NEMU 3020202295 eric Linux ubuntu 5.11.0-25-generic #27-Ubuntu SMP Fri Jul 9 23:06:29 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  06:02:00 up 13:17,  1 user,  load average: 0.27, 0.13, 0.06 96e8206a36d40419e7468821cd1a3f847c04f1b

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -29,7 +29,7 @@ static struct rule {
 	{"/", '/'},					// devide
 	{"\\(", '('},					// left
   {"\\$[a-zA-Z]{2,3}",REG}, //register
-  {"(-)*[0-9]*",NUM}, //decimal number
+  {"(-)*[0-9]+",NUM}, //decimal number
   {"(-)*0[xX][0-9a-fA-F]+",HEXNUM},
 	{"\\)", ')'},					// right 
   {"!=",NEQ},
