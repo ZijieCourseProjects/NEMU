0af64b13b5088f966835ea9fe4ce14dd7be839b6
tracer-NEMU2020 <tjuics2020@163.com>
2021-08-28 07:49:54 -0700

    >  compile NEMU 3020202295 eric Linux ubuntu 5.11.0-25-generic #27-Ubuntu SMP Fri Jul 9 23:06:29 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  07:49:54 up 2 days,  8:14,  1 user,  load average: 0.16, 0.05, 0.01 770688ee0b48378cfed8afbec728d0e751b55d8

--- nemu/src/cpu/exec/logic/cmp-template.h
+++ nemu/src/cpu/exec/logic/cmp-template.h
@@ -3,10 +3,10 @@
 #define instr cmp
 
 static void do_execute(){
-    int32_t result=op_dest->val-op_src->val;
-    update_eflags_pf_zf_sf(result);
-    cpu.eflags.CF=(unsigned)(op_dest->val)>(unsigned)(result);
-    cpu.eflags.OF=((int32_t)(op_dest->val<(int32_t)op_src->val)&&(result>0))||(((int32_t)op_dest->val>(int32_t)op_src->val)&&result<0);
+    DATA_TYPE result=op_dest->val-op_src->val;
+    update_eflags_pf_zf_sf((DATA_TYPE_S)result);
+      cpu.eflags.CF = result > op_dest->val;
+      cpu.eflags.OF = MSB((op_dest->val ^ op_src->val) & (op_dest->val ^ result));
     Log("ZF:%d,SF:%d,CF:%d,OF:%d",cpu.eflags.ZF, cpu.eflags.SF, cpu.eflags.CF, cpu.eflags.OF);
 }
 
