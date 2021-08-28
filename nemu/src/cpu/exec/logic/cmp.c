4d8c21c840b42b054543beb5d17d120d65bf17ef
tracer-NEMU2020 <tjuics2020@163.com>
2021-08-28 08:17:00 -0700

    >  compile NEMU 3020202295 eric Linux ubuntu 5.11.0-25-generic #27-Ubuntu SMP Fri Jul 9 23:06:29 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  08:17:00 up 2 days,  8:41,  1 user,  load average: 0.07, 0.03, 0.00 e7acd621a3e1a25fc185d8e11286365bc5ae0

--- nemu/src/cpu/exec/logic/cmp.c
+++ nemu/src/cpu/exec/logic/cmp.c
@@ -1,9 +1,5 @@
 #include "cpu/exec/helper.h"
 
-#define DATA_BYTE 1
-#include "cmp-template.h"
-#undef DATA_BYTE
-
 
 #define DATA_BYTE 2
 #include "cmp-template.h"
@@ -14,5 +10,5 @@
 #include "cmp-template.h"
 #undef DATA_BYTE
 
-make_helper_v(cmp_i2rm)
+make_helper_v(cmp_si2rm)
 make_helper_v(cmp_r2rm)
--- nemu/src/cpu/exec/logic/cmp.h
+++ nemu/src/cpu/exec/logic/cmp.h
@@ -1,7 +1,7 @@
 #ifndef __CMP_H__
 #define __CMP_H__
 
-make_helper(cmp_i2rm_v);
+make_helper(cmp_si2rm_v);
 make_helper(cmp_r2rm_v);
 make_helper(cmp_r2rm_b);
 make_helper(cmp_i2rm_b);
