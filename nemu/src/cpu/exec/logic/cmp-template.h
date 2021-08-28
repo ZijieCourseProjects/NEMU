4585a04b2909df89638ad2aee3f08b8489b5d8f9
tracer-NEMU2020 <tjuics2020@163.com>
2021-08-28 06:53:50 -0700

    >  compile NEMU 3020202295 eric Linux ubuntu 5.11.0-25-generic #27-Ubuntu SMP Fri Jul 9 23:06:29 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  06:53:49 up 2 days,  7:18,  1 user,  load average: 0.02, 0.02, 0.00 db2649793ec23241a17178ef99007cb43f4f1641

--- nemu/src/cpu/exec/logic/cmp-template.h
+++ nemu/src/cpu/exec/logic/cmp-template.h
@@ -7,6 +7,7 @@ static void do_execute(){
     update_eflags_pf_zf_sf(result);
     cpu.eflags.CF=(unsigned)(op_dest->val)>(unsigned)(result);
     cpu.eflags.OF=((op_dest->val<op_src->val)&&(result>0))||((op_dest->val>op_src->val)&&result<0);
+    Log("ZF:%d,SF:%d,CF:%d,OF:%d",cpu.eflags.ZF, cpu.eflags.SF, cpu.eflags.CF, cpu.eflags.OF);
 }
 
 make_instr_helper(i2rm)
