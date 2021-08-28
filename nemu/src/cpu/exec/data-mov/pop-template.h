de67ba6767cae43ac441b013e1682cc142969c22
tracer-NEMU2020 <tjuics2020@163.com>
2021-08-27 18:27:32 -0700

    >  compile NEMU 3020202295 eric Linux ubuntu 5.11.0-25-generic #27-Ubuntu SMP Fri Jul 9 23:06:29 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  18:27:32 up 1 day, 18:52,  1 user,  load average: 0.08, 0.08, 0.02 7870400ef6d78ebb91bc92b657c67dd1f3a5f741

--- nemu/src/cpu/exec/data-mov/pop-template.h
+++ nemu/src/cpu/exec/data-mov/pop-template.h
@@ -3,6 +3,7 @@
 #define instr pop
 
 static void do_execute(){
+    Log("%x,%x",REG(op_src->val),cpu.ebp);
     if(ops_decoded.is_operand_size_16){
         MEM_W(REG(op_src->val), MEM_R(cpu.esp));
         cpu.esp+=2;
