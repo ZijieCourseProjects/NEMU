da6fffa8d21c619f7819f25535052bfdfa9ec6d8
tracer-NEMU2020 <tjuics2020@163.com>
2021-08-28 21:37:30 -0700

    >  compile NEMU 3020202295 eric Linux ubuntu 5.11.0-25-generic #27-Ubuntu SMP Fri Jul 9 23:06:29 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  21:37:30 up 2 days,  9:32,  1 user,  load average: 0.07, 0.07, 0.02 40866f43d26d0419efebd3f88a1fa0ba6d6ee7e5

--- nemu/src/cpu/exec/logic/test-template.h
+++ nemu/src/cpu/exec/logic/test-template.h
@@ -3,10 +3,12 @@
 #define instr test
 
 static void do_execute(){
-    update_eflags_pf_zf_sf(op_src->val&op_src2->val);
-    Log("%d",cpu.eflags.ZF);
+    update_eflags_pf_zf_sf(op_src->val&op_dest->val);
+    cpu.eflags.CF=0;
+    cpu.eflags.OF=0;
+    print_asm_template2();
 }
 
-make_instr_helper(rm)
+make_instr_helper(r2rm)
 
 #include "cpu/exec/template-end.h"
