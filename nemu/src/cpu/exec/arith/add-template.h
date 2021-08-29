86fe0345999abd0699823195cffc0e726cfe6a58
tracer-NEMU2020 <tjuics2020@163.com>
2021-08-29 05:39:36 -0700

    >  compile NEMU 3020202295 eric Linux ubuntu 5.11.0-25-generic #27-Ubuntu SMP Fri Jul 9 23:06:29 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  05:39:36 up 2 days, 17:30,  1 user,  load average: 0.07, 0.05, 0.01 dee66f858f831ff6274648227135d51dbd853502

--- nemu/src/cpu/exec/arith/add-template.h
+++ nemu/src/cpu/exec/arith/add-template.h
@@ -5,7 +5,7 @@
 static void do_execute(){
     DATA_TYPE result = op_dest->val + op_src ->val;
     OPERAND_W(op_dest, result);
-   
+
     update_eflags_pf_zf_sf((DATA_TYPE_S)result);
     cpu.eflags.CF = result < op_dest->val;
     cpu.eflags.OF = MSB((op_dest->val & op_src->val) & (op_dest->val ^ result));
@@ -19,5 +19,22 @@ make_instr_helper(si2rm)
 
 make_instr_helper(r2rm)
 make_instr_helper(rm2r)
+
+#if DATA_BYTE == 2
+  make_helper(add_eax){
+    int32_t simm =instr_fetch(eip+1,4);
+    DATA_TYPE result = cpu.eax+simm;
+    cpu.eax=result;
+
+    update_eflags_pf_zf_sf((DATA_TYPE_S)result);
+    cpu.eflags.CF = result < op_dest->val;
+    cpu.eflags.OF = MSB((op_dest->val & op_src->val) & (op_dest->val ^ result));
+
+    print_asm_template2();
+    return 5;
+  }
+#endif
+
+
 
 #include "cpu/exec/template-end.h"
--- nemu/src/cpu/exec/arith/add.h
+++ nemu/src/cpu/exec/arith/add.h
@@ -5,5 +5,5 @@ make_helper(add_i2rm_v);
 make_helper(add_si2rm_v);
 make_helper(add_r2rm_v);
 make_helper(add_rm2r_v);
-
+make_helper(add_eax);
 #endif
