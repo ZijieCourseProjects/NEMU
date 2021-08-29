460c37794b69e04d1ebcfcf6a70cbbb174671a96
tracer-NEMU2020 <tjuics2020@163.com>
2021-08-28 23:01:46 -0700

    >  compile NEMU 3020202295 eric Linux ubuntu 5.11.0-25-generic #27-Ubuntu SMP Fri Jul 9 23:06:29 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  23:01:46 up 2 days, 10:56,  1 user,  load average: 0.03, 0.03, 0.00 dc88a93475c92432468750ad1420725286c94833

--- nemu/src/cpu/exec/data-mov/setne-template.h
+++ nemu/src/cpu/exec/data-mov/setne-template.h
@@ -5,9 +5,9 @@
 static void do_execute(){
     Log("%d",cpu.eflags.ZF);
     if(!cpu.eflags.ZF){
-        op_dest->val=1;
+        OPERAND_W(op_dest, 1);
     }else{
-        op_dest->val=0;
+        OPERAND_W(op_dest, 0);
         Log("val:%x",op_src->val);
     }
     print_asm_template1();
