6a10a9bce33976e1d403bad7e1a37693ab927de8
tracer-NEMU2020 <tjuics2020@163.com>
2021-08-29 04:12:01 -0700

    >  compile NEMU 3020202295 eric Linux ubuntu 5.11.0-25-generic #27-Ubuntu SMP Fri Jul 9 23:06:29 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  04:12:01 up 2 days, 16:07,  1 user,  load average: 0.01, 0.04, 0.04 a7310ab2d17ecd033d41e6def12d24f2aa73d172

--- nemu/src/cpu/exec/logic/cmp-template.h
+++ nemu/src/cpu/exec/logic/cmp-template.h
@@ -3,7 +3,7 @@
 #define instr cmp
 
 static void do_execute(){
-    DATA_TYPE result=op_dest->val-op_src->val;
+    DATA_TYPE result=op_src->val-op_dest->val;
     update_eflags_pf_zf_sf((DATA_TYPE_S)result);
       cpu.eflags.CF = result > op_dest->val;
       cpu.eflags.OF = MSB((op_dest->val ^ op_src->val) & (op_dest->val ^ result));
