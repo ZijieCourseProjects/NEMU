cdab14a60eb1f986cade37f9bbcd5758d2af29f1
tracer-NEMU2020 <tjuics2020@163.com>
2021-08-29 06:47:56 -0700

    >  compile NEMU 3020202295 eric Linux ubuntu 5.11.0-25-generic #27-Ubuntu SMP Fri Jul 9 23:06:29 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  06:47:56 up 2 days, 18:38,  1 user,  load average: 0.09, 0.16, 0.88 b67c0ee74b575df617ae62bb51c04fce41d578e4

--- nemu/src/cpu/exec/control/ret-template.h
+++ nemu/src/cpu/exec/control/ret-template.h
@@ -1,6 +1,5 @@
 #include "cpu/exec/template-start.h"
 
-#if DATA_BYTE == 4
 make_helper(ret){
     if(ops_decoded.is_operand_size_16){
         cpu.eip=MEM_R(cpu.esp)&0x0000FFFF;
@@ -12,9 +11,6 @@ make_helper(ret){
     print_asm("ret");
     return 0;
 }
-#endif
-
-#if DATA_BYTE == 2
 make_helper(ret_imm){
     int16_t imm=instr_fetch(eip+1, 2);
     cpu.eip=MEM_R(cpu.esp);
@@ -25,6 +21,5 @@ make_helper(ret_imm){
     print_asm("ret");
     return 0;
 }
-#endif
 
 #include "cpu/exec/template-end.h"
--- nemu/src/cpu/exec/control/ret.c
+++ nemu/src/cpu/exec/control/ret.c
@@ -3,7 +3,3 @@
 #define DATA_BYTE 4
 #include "ret-template.h"
 #undef DATA_BYTE
-
-#define DATA_BYTE 2
-#include "ret-template.h"
-#undef DATA_BYTE
