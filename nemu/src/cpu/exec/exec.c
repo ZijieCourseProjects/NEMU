1c21fbbdc91ea5c7778a4fbaa93756f8d952c4b9
tracer-NEMU2020 <tjuics2020@163.com>
2021-08-28 23:43:40 -0700

    >  compile NEMU 3020202295 eric Linux ubuntu 5.11.0-25-generic #27-Ubuntu SMP Fri Jul 9 23:06:29 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  23:43:40 up 2 days, 11:38,  1 user,  load average: 0.05, 0.03, 0.00 d750e0dd41040463e8eb3395fc3cd901bbf904d3

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -109,7 +109,7 @@ helper_fun opcode_table [256] = {
 /* 0x30 */	inv, xor_r2rm_v, inv, inv,
 /* 0x34 */	inv, inv, inv, inv,
 /* 0x38 */	inv, cmp_r2rm_v, inv, inv,
-/* 0x3c */	inv, inv, inv, inv,
+/* 0x3c */	cmp_si2rm_v, inv, inv, inv,
 /* 0x40 */	inc_r_v, inc_r_v, inc_r_v, inc_r_v,
 /* 0x44 */	inv, inc_r_v, inc_r_v, inc_r_v,
 /* 0x48 */	dec_r_v, dec_r_v, dec_r_v, dec_r_v,
