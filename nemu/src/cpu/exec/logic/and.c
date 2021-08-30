b2decbbeb7f1ec70edf6ef1ddd38900776626d22
tracer-NEMU2020 <tjuics2020@163.com>
2021-08-30 07:52:40 +0000

    >  compile NEMU 3020202295 simpleedu Linux simpleedu 4.15.0-154-generic #161-Ubuntu SMP Fri Jul 30 13:04:17 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  07:52:40 up 2 days,  1:13,  1 user,  load average: 0.10, 0.12, 0.10 35cef4c86ef37c4f0b9185db44d88baf6647b1

--- nemu/src/cpu/exec/logic/and.c
+++ nemu/src/cpu/exec/logic/and.c
@@ -1,16 +1,22 @@
 #include "cpu/exec/helper.h"
 
 #define DATA_BYTE 1
+#define REGI AL
 #include "and-template.h"
 #undef DATA_BYTE
+#undef REGI
 
 #define DATA_BYTE 2
+#define REGI AX
 #include "and-template.h"
 #undef DATA_BYTE
+#undef REGI
 
 #define DATA_BYTE 4
+#define REGI EAX
 #include "and-template.h"
 #undef DATA_BYTE
+#undef REGI
 
 /* for instruction encoding overloading */
 
