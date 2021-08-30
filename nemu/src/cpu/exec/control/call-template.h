4350e0699d5689c09329e1b8f0abe029347ffee8
tracer-NEMU2020 <tjuics2020@163.com>
2021-08-30 10:31:05 +0000

    >  compile NEMU 3020202295 simpleedu Linux simpleedu 4.15.0-154-generic #161-Ubuntu SMP Fri Jul 30 13:04:17 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  10:31:05 up 2 days,  3:51,  1 user,  load average: 0.16, 0.04, 0.01 b24c50dee1b051f67afb297ce3a5a160124e86f0

--- nemu/src/cpu/exec/control/call-template.h
+++ nemu/src/cpu/exec/control/call-template.h
@@ -17,13 +17,13 @@ static void do_execute(){
 make_instr_helper(i)
 
 make_helper(concat(call_rm_,SUFFIX)){
-    concat(decode_rm_,SUFFIX)(eip+1);
+    int len=concat(decode_rm_,SUFFIX)(eip+1);
     if(ops_decoded.is_operand_size_16){
         cpu.esp-=DATA_BYTE;
-        MEM_W(cpu.esp,cpu.eip+DATA_BYTE+1);
+        MEM_W(cpu.esp,cpu.eip+len+1);
     }else{
         cpu.esp-=DATA_BYTE;
-        MEM_W(cpu.esp, cpu.eip+DATA_BYTE+1);
+        MEM_W(cpu.esp, cpu.eip+len+1);
     }
         cpu.eip=op_src->val;
         return 0;
