90ec14962920e12dd489000ddb59b5bb377215bc
tracer-NEMU2020 <tjuics2020@163.com>
2021-09-02 12:39:50 +0000

    sideops2-3

--- lib-common/FLOAT/FLOAT_vfprintf.c
+++ lib-common/FLOAT/FLOAT_vfprintf.c
@@ -16,9 +16,20 @@ __attribute__((used)) static int format_FLOAT(FILE *stream, FLOAT f)
 	 *         0x00010000    "1.000000"
 	 *         0x00013333    "1.199996"
 	 */
-
+	char si = (f>>31) & 0x1?'-':'\0';
+	if(si)
+		f*=-1;
+	int intNum=f>>16;
+	int helper[]={0, 500000000, 250000000, 125000000, 62500000, 31250000, 15625000, 7812500, 3906250, 1953125, 976562, 488281, 244140, 122070, 61035, 30517, 15258, };
+	int i,fruc;
+	for(i=1;i<=16;i++){
+		if((f & 0xffff)& (1 << (16-i))){
+			fruc+=helper[i];
+		}
+	}
+	fruc/=1000;
 	char buf[80];
-	int len = sprintf(buf, "0x%08x", f);
+	int len = sprintf(buf, "%c%d.%06u", si,intNum,fruc);
 	return __stdio_fwrite(buf, len, stream);
 }
 
