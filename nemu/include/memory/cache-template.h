b1b1e400641c82fbd24eb37292a0e41fa445bbc9
tracer-NEMU2020 <tjuics2020@163.com>
2021-09-04 04:45:32 +0000

    >  compile NEMU 3020202295 simpleedu Linux simpleedu 4.15.0-154-generic #161-Ubuntu SMP Fri Jul 30 13:04:17 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  04:45:32 up 6 days, 22:06,  2 users,  load average: 0.22, 0.06, 0.03 ceac2bfe92207c065b9e406c8356690adf436a8

--- nemu/include/memory/cache-template.h
+++ nemu/include/memory/cache-template.h
@@ -13,6 +13,13 @@ typedef struct CacheLine
 
 typedef struct Cache
 {
+    int blockSize;
+    int size;
+    int blockBit;
+    int groupBit;
+    int wayBit;
+    int groupSize;
+    int waySize;
     CacheLine lines[CACHE_SIZE/CACHE_BLOCK_SIZE];
 
     void (* init)(struct Cache *this);
@@ -22,6 +29,14 @@ typedef struct Cache
 
 void concat(init_,CACHENAME)(struct Cache *this){
     int i=0;
+    this->blockSize=CACHE_BLOCK_SIZE;
+    this->size=CACHE_SIZE;
+    this->wayBit=CACHE_WAY_BIT;
+    this->groupBit=CACHE_GROUP_BIT;
+    this->blockBit=CACHE_BLOCK_BIT;
+    this->groupSize=(1 << CACHE_GROUP_BIT);
+    this->waySize=(1<<CACHE_WAY_BIT);
+
     for(;i<CACHE_SIZE/CACHE_BLOCK_SIZE;i++){
         (this->lines[i]).valid=false;
         #ifdef DIRTY
--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -2,11 +2,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+
 #define CACHENAME cacheL1
 #define CACHE_BLOCK_SIZE 64
 #define CACHE_SIZE 64*1024
+#define CACHE_WAY_BIT 3
+#define CACHE_GROUP_BIT 7
+#define CACHE_BLOCK_BIT 6
 
-#include "memory/cache.h"
+#include "memory/cache-template.h"
 
 Cache cacheL1;
 
@@ -14,8 +19,34 @@ Cache cacheL1;
 #undef CACHENAME
 #undef CACHE_BLOCK_SIZE
 #undef CACHE_SIZE
+#undef CACHE_WAY_BIT
+#undef CACHE_GROUP_BIT
+#undef CACHE_BLOCK_BIT
+
+
 
 void initCache(){
     cacheL1.init=init_cacheL1;
     cacheL1.init(&cacheL1);
+}
+
+int read_cache_hit(hwaddr_t addr,Cache *this){
+    uint32_t groupIndex = (addr >> this->blockBit) & (this->groupSize -1);
+    uint32_t tag = (addr >> (this->groupSize+this->blockBit));
+
+    int i,group=groupIndex*this->waySize;
+    for(i = group;i<group+this->waySize;i++){
+        if(this->lines[i].valid==true && this->lines[i].tag==tag){
+            return i;
+        }
+    }
+    return -1;
+}
+
+int read_cacheL1(hwaddr_t addr){
+    int ans = read_cache_hit(addr,&cacheL1);
+    if(ans!=-1){
+        return ans; 
+    }
+    return 0;
 }
