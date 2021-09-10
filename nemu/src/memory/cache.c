5704ccc809ac29e1efe616bbe0fc5d1c0fa78da2
3020202295-ZHAOZIJIE <veweew@tju.edu.cn>
2021-09-10 18:42:32 +0800

    fixed a bug of cache

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -124,26 +124,48 @@ void write_cacheL1(hwaddr_t addr,size_t len,uint32_t data){
     uint32_t offset = addr & (cacheL1.blockSize - 1);
 
     int i,group = group_idx * cacheL1.waySize;
-    for (i = group + 0;i < group + cacheL1.waySize;i ++){
+
+	if(offset+len > cacheL1.blockSize){
+	  write_cacheL1(addr + cacheL1.blockSize - offset,len - (cacheL1.blockSize - offset),data >>(8*(cacheL1.blockSize - offset)));
+	}
+	for(i = group+0;i<group+cacheL1.waySize;i++){
+	  if(cacheL1.lines[i].valid && cacheL1.lines[i].tag==tag){
+		memcpy(cacheL1.lines[i].data + offset, &data, (len<cacheL1.blockSize-offset)?len:cacheL1.blockSize-offset);
+		write_cacheL2(addr,(len<cacheL1.blockSize-offset)?len:cacheL1.blockSize-offset,data);
+		return;
+	  }
+	}
+	write_cacheL2(addr,len,data);
+/*
+  for (i = group + 0;i < group + cacheL1.waySize;i ++){
         if (cacheL1.lines[i].valid == 1 && cacheL1.lines[i].tag == tag){// WRITE HIT
-            /*write through*/
+            */
+/*write through*//*
+
             if (offset + len > cacheL1.blockSize){
-                dram_write(addr,cacheL1.blockSize - offset,data);
                 memcpy(cacheL1.lines[i].data + offset, &data, cacheL1.blockSize - offset);
-                /*Update Cache2*/
+                */
+/*Update Cache2*//*
+
                 write_cacheL2(addr,cacheL1.blockSize - offset,data);
 
                 write_cacheL1(addr + cacheL1.blockSize - offset,len - (cacheL1.blockSize - offset),data >>(8*(cacheL1.blockSize - offset)));
             }else {
-                dram_write(addr,len,data);
                 memcpy(cacheL1.lines[i].data + offset, &data, len);
-                /*Update Cache2*/
+                */
+/*Update Cache2*//*
+
                 write_cacheL2(addr,len,data);
             }
             return;
         }
     }
+	if(offset+len>cacheL1.blockSize){
+	  cacheL1
+	}
     write_cacheL2(addr,len,data);
+
+*/
 }
 void write_cacheL2(hwaddr_t addr, size_t len, uint32_t data){
     uint32_t group_idx = (addr >> cacheL2.blockBit) & (cacheL2.groupSize - 1);
